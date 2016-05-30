46d7cbd73fc3428ab0c88bcfd5c5effe70920821
hsoudry <helenesoudry@gmail.com>
2016-05-30 15:43:57 +0100

    added user authentification for queries index

--- app/Http/Controllers/QueryController.php
+++ app/Http/Controllers/QueryController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
+
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -10,9 +12,16 @@ use App\Query;
 
 class QueryController extends Controller
 {
+
+  public function __construct() {
+      $this->middleware('auth');
+  }
+
     public function index() {
-      $queries = Query::all();
-      
+
+      $user = Auth::user();
+      $queries = Query::all()->where('user_id',$user->id);
+
       return view('queries.index', compact('queries'));
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
 
     <!-- Styles -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
     {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
 
     <style>
@@ -24,6 +25,7 @@
             margin-right: 6px;
         }
     </style>
+    <link rel="stylesheet" href="public/css/personalCSS.css" type="text/css">
 </head>
 <body id="app-layout">
     <nav class="navbar navbar-default navbar-static-top">
