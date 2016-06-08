de3e2b5ab30290c4b04a646fa54d1c2ab55f260d
hsoudry <helene.soudry@insa-rouen.fr>
2016-06-08 19:17:11 +0100

    added 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Page not found</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                color: #7b929d;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 72px;
+                margin-bottom: 40px;
+                tex
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">Sorry the page you are looking for cannot be found. <br>Would you like to redirect to the <a href="/">welcome page</a> instead ?</div>
+            </div>
+        </div>
+    </body>
+</html>
