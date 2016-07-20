92f1fc03141c71b5b05b7b2c61a8bbafaa29ab15
hsoudry <helene.soudry@insa-rouen.fr>
2016-07-20 12:04:51 +0100

    Moved the helper function buildQuery1

--- app/Http/Controllers/QueryController.php
+++ app/Http/Controllers/QueryController.php
@@ -52,7 +52,7 @@ class QueryController extends Controller
       $path = $request->input('filename');
       $query_time = Carbon::parse($request->input('query_date').' '.$request->input('query_time'));
 
-      $result = $this->buildQuery1($query_time->toDateTimeString(), $path);
+      $result = buildQuery1($query_time->toDateTimeString(), $path);
 
       Query::create(array(
                             'user_id' => $user->id,
@@ -77,73 +77,6 @@ class QueryController extends Controller
                           ));
 
       return redirect('queries')->with('status','UPDATED');
-    }
-
-    private function buildQuery1($query_time, $path) {
-      $client = AWS::createClient('EMR');
-
-      $result = $client->runJobFlow([
-        'AmiVersion' => '4.7.1',
-        'Applications' => [
-          [
-            'Name' => 'Hadoop',
-            'Version' => '2.7.2',
-          ],
-        ],
-        'Instances' => [
-          'Ec2KeyName' => 'EMR key',
-          'InstanceGroups' => [
-            [
-              'Configurations' => [
-                [
-                  'Classification' => 'hadoop-env',
-                  'Configurations' => [
-                    'Classification' => 'export',
-                    'Configurations' => [],
-                    'Properties' => ['JAVA_HOME' => '/usr/lib/jvm/java-1.8.0'],
-                  ],
-                  'Properties' => [],
-                ],
-              ],
-              'InstanceCount' => 1,
-              'InstanceRole' => 'MASTER',
-              'InstanceType' => 'm3.xlarge',
-              'Name' => 'Master instance group - 1',
-            ],
-            [
-              'Configurations' => [
-                [
-                  'Classification' => 'hadoop-env',
-                  'Configurations' => [
-                    'Classification' => 'export',
-                    'Configurations' => [],
-                    'Properties' => ['JAVA_HOME' => '/usr/lib/jvm/java-1.8.0'],
-                  ],
-                  'Properties' => [],
-                ],
-              ],
-              'InstanceCount' => 2,
-              'InstanceRole' => 'CORE',
-              'InstanceType' => 'm3.xlarge',
-              'Name' => 'Core instance group - 2',
-            ],
-          ],
-        ],
-        'Name' => 'Query 1 cluster',
-        'Steps' => [
-          [
-            'ActionOnFailure' => 'CONTINUE',
-            'HadoopJarStep' => [
-              'Args' => ['s3://thesisdata/input', 's3://thesisdata/output/'+$path, $query_time],
-              'Jar' => 's3://thesisdata/jar/TopTenRoutes.jar',
-              'MainClass' => 'TopTenRoutes',
-            ],
-            'LogUri' => 's3://thesisdata/logs/',
-            'Name' => 'Top ten routes computation',
-          ],
-        ],
-      ]);
-      return $result;
     }
 
 }
---
+++ app/Libraries/helperFunction.php
@@ -0,0 +1,71 @@
+<?php
+  if(!function_exists(buildQuery1)) {
+    
+    function buildQuery1($query_time, $path) {
+      $client = AWS::createClient('EMR');
+
+      $result = $client->runJobFlow([
+        'AmiVersion' => '4.7.1',
+        'Applications' => [
+          [
+            'Name' => 'Hadoop',
+            'Version' => '2.7.2',
+          ],
+        ],
+        'Instances' => [
+          'Ec2KeyName' => 'EMR key',
+          'InstanceGroups' => [
+            [
+              'Configurations' => [
+                [
+                  'Classification' => 'hadoop-env',
+                  'Configurations' => [
+                    'Classification' => 'export',
+                    'Configurations' => [],
+                    'Properties' => ['JAVA_HOME' => '/usr/lib/jvm/java-1.8.0'],
+                  ],
+                  'Properties' => [],
+                ],
+              ],
+              'InstanceCount' => 1,
+              'InstanceRole' => 'MASTER',
+              'InstanceType' => 'm3.xlarge',
+              'Name' => 'Master instance group - 1',
+            ],
+            [
+              'Configurations' => [
+                [
+                  'Classification' => 'hadoop-env',
+                  'Configurations' => [
+                    'Classification' => 'export',
+                    'Configurations' => [],
+                    'Properties' => ['JAVA_HOME' => '/usr/lib/jvm/java-1.8.0'],
+                  ],
+                  'Properties' => [],
+                ],
+              ],
+              'InstanceCount' => 2,
+              'InstanceRole' => 'CORE',
+              'InstanceType' => 'm3.xlarge',
+              'Name' => 'Core instance group - 2',
+            ],
+          ],
+        ],
+        'Name' => 'Query 1 cluster',
+        'Steps' => [
+          [
+            'ActionOnFailure' => 'CONTINUE',
+            'HadoopJarStep' => [
+              'Args' => ['s3://thesisdata/input', 's3://thesisdata/output/'+$path, $query_time],
+              'Jar' => 's3://thesisdata/jar/TopTenRoutes.jar',
+              'MainClass' => 'TopTenRoutes',
+            ],
+            'LogUri' => 's3://thesisdata/logs/',
+            'Name' => 'Top ten routes computation',
+          ],
+        ],
+      ]);
+      return $result;
+    }
+  }
+ ?>
