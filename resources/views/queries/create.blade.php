99fc46e9895e2ef9baf37ebe90f40d3439f501aa
hsoudry <helene.soudry@insa-rouen.fr>
2016-08-14 08:24:47 +0100

    css update

--- resources/views/queries/create.blade.php
+++ resources/views/queries/create.blade.php
@@ -1,55 +1,56 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="row">
-  <div class="col-xs-12 col-sm-offset-1 col-sm-10">
-    <h1>Create a new Query</h1>
-    <hr/>
-    {!! Form::open(array('action' => 'QueryController@store')) !!}
+<div class="container">
+  <div class="row">
+    <div class="col-xs-12">
+      <h1>Create a new Query</h1>
+      <hr/>
+      {!! Form::open(array('action' => 'QueryController@store')) !!}
 
-    <div class="form-group">
-      <div class="row">
-        <div class="col-sm-6">
-          {!! Form::radio('query_type','1',true, array('class' => 'radio-inline','style'=>'vertical-align: middle; margin: 0px;')) !!}
-          {!! Form::label('1','Query 1') !!}
+      <div class="form-group">
+        <div class="row">
+          <div class="col-sm-6">
+            {!! Form::radio('query_type','1',true, array('class' => 'radio-inline','style'=>'vertical-align: middle; margin: 0px;')) !!}
+            {!! Form::label('1','Query 1') !!}
+          </div>
+          <div class="col-sm-6">
+            {!! Form::radio('query_type','2', false, array('class' => 'radio-inline','style'=>'vertical-align: middle; margin: 0px;')) !!}
+            {!! Form::label('2','Query 2') !!}
+          </div>
         </div>
-        <div class="col-sm-6">
-          {!! Form::radio('query_type','2', false, array('class' => 'radio-inline','style'=>'vertical-align: middle; margin: 0px;')) !!}
-          {!! Form::label('2','Query 2') !!}
-        </div>
-      </div>
 
 
-  </div>
-
-    <div class="form-group">
-      {!! Form::label('query_date','Query search date:') !!}
-      {!! Form::date('query_date', \Carbon\Carbon::createFromDate(2013,1,1), array('class'=>'form-control')) !!}<br>
-      {!! Form::label('query_date','Query search time:') !!}
-      {!! Form::time('query_time', '12:00' , array('class'=>'form-control')) !!}
     </div>
 
-  <div class="form-group">
-    {!! Form::label('filename','Output file name:') !!}
-    {!! Form::text('filename','output_'.Auth::user()->name.'_'.\Carbon\Carbon::now()->timestamp, array('class'=>'form-control')) !!}
-  </div>
-
-
-    {!! Form::submit('Submit', array('class' => 'btn btn-primary form-control')) !!}
-    {!! Form::close() !!}
-    <br>
-    @if ($errors->any())
-    <div class="form-group">
-      <div class=" alert alert-danger">
-        <ul>
-          @foreach($errors->all() as $error)
-            <li>{{$error}}</li>
-          @endforeach
-        </ul>
+      <div class="form-group">
+        {!! Form::label('query_date','Query search date:') !!}
+        {!! Form::date('query_date', \Carbon\Carbon::createFromDate(2013,1,1), array('class'=>'form-control')) !!}<br>
+        {!! Form::label('query_date','Query search time:') !!}
+        {!! Form::time('query_time', '12:00' , array('class'=>'form-control')) !!}
       </div>
+
+    <div class="form-group">
+      {!! Form::label('filename','Output file name:') !!}
+      {!! Form::text('filename','output_'.Auth::user()->name.'_'.\Carbon\Carbon::now()->timestamp, array('class'=>'form-control')) !!}
     </div>
-    @endif
+
+
+      {!! Form::submit('Submit', array('class' => 'btn btn-primary form-control')) !!}
+      {!! Form::close() !!}
+      <br>
+      @if ($errors->any())
+      <div class="form-group">
+        <div class=" alert alert-danger">
+          <ul>
+            @foreach($errors->all() as $error)
+              <li>{{$error}}</li>
+            @endforeach
+          </ul>
+        </div>
+      </div>
+      @endif
+    </div>
   </div>
 </div>
-
 @stop
--- resources/views/queries/index.blade.php
+++ resources/views/queries/index.blade.php
@@ -12,50 +12,52 @@
     </p>
 </div>
 @endif
-<div class="row">
-  <div class="col-sm-offset-2 col-xs-12 col-sm-8">
-         <div class="panel panel-default">
-          <div class="panel-heading">
-            <h4>Your Queries</h4>
-            </div>
-            <table class="table table-striped">
-              <thead>
+<div class="container">
+  <div class="row">
+    <div class="col-xs-12">
+           <div class="panel panel-default">
+            <div class="panel-heading">
+              <h4>Your Queries</h4>
+              </div>
+              <table class="table table-striped">
+                <thead>
+                  <tr>
+                    <th>Query type</th>
+                    <th>Cluster status</th>
+                    <th>Request time</th>
+                    <th>Completion time</th>
+                    <th>Results</th>
+                  </tr>
+                </thead>
+                <tbody>
+                @foreach($queries as $query)
                 <tr>
-                  <th>Query type</th>
-                  <th>Cluster status</th>
-                  <th>Request time</th>
-                  <th>Completion time</th>
-                  <th>Results</th>
-                </tr>
-              </thead>
-              <tbody>
-              @foreach($queries as $query)
-              <tr>
-                <td>{{$query->query_type}}</td>
-                <td>{{$query->status}}</td>
-                <td>{{$query->created_at}}</td>
-                <td>
-                  @if($query->completion_time=='')
-                    @if(($query->status == 'TERMINATED' || ($query->status == 'TERMINATED_WITH_ERRORS')))
-                      NOT AVAILABLE
-                    @else
+                  <td>{{$query->query_type}}</td>
+                  <td>{{$query->status}}</td>
+                  <td>{{$query->created_at}}</td>
+                  <td>
+                    @if($query->completion_time=='')
+                      @if(($query->status == 'TERMINATED' || ($query->status == 'TERMINATED_WITH_ERRORS')))
+                        NOT AVAILABLE
+                      @else
 
+                      @endif
+                    @else
+                      {!!Carbon\Carbon::parse($query->completion_time)->format('Y-m-d H:i:s')!!}
                     @endif
-                  @else
-                    {!!Carbon\Carbon::parse($query->completion_time)->format('Y-m-d H:i:s')!!}
-                  @endif
-                </td>
-                <td>
-                  @if($query->status == 'TERMINATED')
-                  <a href="{{ URL::to('https://thesisdata.s3.amazonaws.com/output/'.$user->name.'/'.$query->path.'/part-r-00000') }}">Download</a>
-                  @endif
-                </td>
-              </tr>
-            @endforeach
-          </tbody>
-          </table>
+                  </td>
+                  <td>
+                    @if($query->status == 'TERMINATED')
+                    <a href="{{ URL::to('https://thesisdata.s3.amazonaws.com/output/'.$user->name.'/'.$query->path.'/part-r-00000') }}">Download</a>
+                    @endif
+                  </td>
+                </tr>
+              @endforeach
+            </tbody>
+            </table>
+          </div>
+          <a href="queries/create"><button type="button" name="newQuery" class="btn btn-primary form-control">Create a new query</button></a>
         </div>
-        <a href="queries/create"><button type="button" name="newQuery" class="btn btn-primary form-control">Create a new query</button></a>
-      </div>
-    </div>
+  </div>
+</div>
 @stop
